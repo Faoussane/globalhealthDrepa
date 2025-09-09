2367f6b9777818dc95f1c2c180d43f25b64613ff
Faoussane <yfaoussane@gmail.com>
2025-09-09 19:27:16 +0100

    Create create_superuser.py

---
+++ create_superuser.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "globalhealth_project.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "Nassif")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "Nassif1234")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser '{username}' created ✅")
+else:
+    print(f"Superuser '{username}' already exists")
